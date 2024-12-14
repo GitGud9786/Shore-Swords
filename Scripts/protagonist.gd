2f1a7bad94d18b300b14967b5aa422cad5977a88
Muhammad Tausif Ul Islam <140267239+GitGud9786@users.noreply.github.com>
2024-12-14 23:37:55 +0600

    Pushing initial progress of the main protagonist

---
+++ Scripts/protagonist.gd
@@ -0,0 +1,74 @@
+extends CharacterBody2D
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var area_collision: Area2D = $AnimatedSprite2D/area_collision
+
+var input_enabled = 1
+
+const SPEED = 120
+#const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	#if not is_on_floor():
+	#	velocity += get_gravity() * delta
+
+	# Handle jump.
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	#	velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var x_direction := Input.get_axis("move_left", "move_right")
+	var y_direction := Input.get_axis("move_up", "move_down")
+	
+	if Input.is_action_just_pressed("attack"):#initiate attack
+		input_enabled= 0
+		area_collision.monitoring=true
+		var attack_type = randi() % 2 + 1
+		animated_sprite.play("knight_attack_"+str(attack_type))
+	
+	if x_direction>0 and input_enabled:#moving right
+		animated_sprite.play("knight_running")
+		animated_sprite.flip_h = false
+		area_collision.scale = abs(area_collision.scale)
+	elif x_direction<0 and input_enabled:#moving left
+		animated_sprite.play("knight_running")
+		animated_sprite.flip_h = true
+		area_collision.scale = -abs(area_collision.scale)
+	
+	if y_direction!=0 and input_enabled:#moving up or down
+		animated_sprite.play("knight_running")
+	
+	if x_direction==0 and y_direction==0 and input_enabled:#fully still
+		animated_sprite.play("knight_idle")
+	
+	if x_direction:
+		velocity.x = x_direction * SPEED * input_enabled
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if y_direction:
+		velocity.y = y_direction * SPEED * input_enabled
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
+	#print(x_direction," ", y_direction," ",input_enabled)
+	
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if animated_sprite.animation == "knight_attack_1" or animated_sprite.animation == "knight_attack_2":
+		input_enabled= 1
+		area_collision.monitoring=false
+		animated_sprite.play("knight_idle")
+
+
+func _on_area_collision_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	print("Collision entered")
+
+
+func _on_area_collision_area_shape_exited(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	print("Collision exited")
