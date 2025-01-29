347b557b1e3267539a2650c1df01a2aa6541a9c8
Muhammad Tausif Ul Islam <140267239+GitGud9786@users.noreply.github.com>
2025-01-29 23:20:14 +0600

    Boss AI going crazy

--- Scripts/boss.gd
+++ Scripts/boss.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-
+var SPEED = 10.0
+var direction = Vector2.ZERO
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,4 +9,13 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	print(direction)
+	velocity = direction * SPEED
+	
+	move_and_slide()
+
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	if body.get_node("protagonist_body_collision"):
+		print("player located")
+		direction =  global_position.direction_to(body.global_position)
