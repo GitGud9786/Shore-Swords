58ff1e57116b672755abcdba8c5754fedcb749ed
Muhammad Tausif Ul Islam <140267239+GitGud9786@users.noreply.github.com>
2025-01-30 23:39:36 +0600

    Fixed movement code only of boss

--- Scripts/boss.gd
+++ Scripts/boss.gd
@@ -1,7 +1,11 @@
 extends CharacterBody2D
 
-var SPEED = 10.0
+@onready var detection_area: Area2D = $detection_area
+
+var SPEED = 30.00
+var detected = false
 var direction = Vector2.ZERO
+var target: Node2D = null
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,13 +13,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print(direction)
+	if target:
+		direction = global_position.direction_to(target.global_position)
+	
 	velocity = direction * SPEED
 	
 	move_and_slide()
 
-
 func _on_detection_area_body_entered(body: Node2D) -> void:
 	if body.get_node("protagonist_body_collision"):
-		print("player located")
+		target = body
 		direction =  global_position.direction_to(body.global_position)
+
+func _on_detection_area_body_exited(body: Node2D) -> void:
+	if body == target:
+		target = null
